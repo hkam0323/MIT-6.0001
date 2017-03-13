5da2b5a11d6ce751f476eb2d8aa70fa373d7cbfd
hkam0323 <hiulam.kam@gmail.com>
2017-03-13 13:07:04 -0500

    Add files via upload

---
+++ 20170210 Problem set 0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 11:51:16 2017
+
+@author: Hiu
+"""
+import math
+x=int(input("Enter number x:"))  #User enters input for var 'x' --> assign to int
+y=int(input("Enter number y:"))  #User enters input for var 'y' --> assign to int
+print("X**y =", x**y)            #Print x**y
+print("log(x) =", int(math.log(x,2)))  #Print log(x) base 2
